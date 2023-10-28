5e941d97c4ee67ca04698476e1ddb0e08403b74d
kamenkremen <ivanz0610@gmail.com>
2023-10-29 00:06:41 +0300

    change binary from int to char

--- BinaryReperesentation/BinaryReperesentation.c
+++ BinaryReperesentation/BinaryReperesentation.c
@@ -6,76 +6,81 @@
 #include <stdbool.h>
 #include <locale.h>
 
-bool isTwoArraysEqual(int* first, int* second, int size)
+enum ERRORCODES
 {
-    for (int i = 0; i < size; i++)
+    ok,
+    inputError,
+};
+const char* TEST_ERRORS[4] = { "ok", "sum fuction", "twos complement function", "to decimal function" };
+
+bool isTwoArraysEqual(const char* const first, const char* const second, const size_t size)
+{
+    for (int i = 0; i < size; ++i)
     {
         if (first[i] != second[i])
         {
+            printf("%c, %c\n", first[i], second[i]);
             return false;
         }
     }
     return true;
 }
 
-void XOREverythingWithOnes(int* binary, int size)
+void XOREverythingWithOnes(char* const binary, const size_t size)
 {
-    for (int i = size - 1; i >= 0; i--)
+    for (size_t i = size - 1; i != -1; --i)
     {
-        binary[i] = (binary[i] + 1) % 2;
+        binary[i] = '0' + ((binary[i] - '0') + 1) % 2;
     }
 }
 
-int myMax(int firstNumber, int secondNumber)
+int myMax(const int firstNumber, const int secondNumber)
 {
-    if (firstNumber >= secondNumber)
-    {
-        return firstNumber;
-    }
-    return secondNumber;
+    return firstNumber > secondNumber ? firstNumber : secondNumber;
 }
 
-void getTwosComplement(int number, int* binary, int size)
+void getTwosComplement(int number, char* const binary, const size_t size)
 {
-    int sign = 0;
-    if (number < 0)
-    {
-        sign = 1;
-    }
+    const int sign = number < 0 ? 1 : 0;
+   // printf("%d, %d\n", number, sign);
     int current = 1;
     number = abs(number);
+    for (size_t i = 0; i < size; ++i)
+    {
+        binary[i] = '0';
+    }
     while (number >= 1)
     {
-        binary[size - current] = number % 2;
+        binary[size - current] = (char)(number % 2) + '0';
         number /= 2;
         ++current;
     }
     if (sign)
     {
-        if (sign)
+        XOREverythingWithOnes(binary, size);
+        for (size_t i = size - 1; i != 0; --i)
         {
-            XOREverythingWithOnes(binary, size);
-            for (int i = size - 1; i >= 0; i--)
+            if (binary[i] == '0')
             {
-                if (binary[i] == 0)
-                {
-                    binary[i] = 1;
-                    return;
-                }
-                binary[i] = 0;
+                binary[i] = '1';
+                binary[size] = '\0';
+                return;
             }
+            binary[i] = '0';
         }
     }
+    binary[size] = '\0';
 }
 
-void getSum(int* firstNumber, int* secondNumber, int* thirdNumber, int size)
+void getSum(const char* const firstNumber, const char* const secondNumber, char* const thirdNumber, const size_t size)
 {
     bool transfered = false;
-    for (int i = size - 1; i >= 0; --i)
+    for (size_t i = size - 1; i != -1; --i)
     {
-        int number = firstNumber[i];
+        int number = firstNumber[i] - '0';
+        
         if (i < size) {
-            number += secondNumber[i];
+            number += secondNumber[i] - '0';
         }
         if (transfered)
         {
@@ -84,10 +89,10 @@ void getSum(int* firstNumber, int* secondNumber, int* thirdNumber, int size)
         }
         if (number >= 2)
         {
-            number %= 2;
+            number = number % 2;
             transfered = true;
         }
-        thirdNumber[i] = number;
+        thirdNumber[i] = number + '0';
     }
 }
 
@@ -95,13 +100,15 @@ int getSumTest(void)
 {
     int firstNumber = 5;
     int secondNumber = -5;
-    int firstNumberBinary[sizeof(int) * 8] = { 0 };
+    char firstNumberBinary[sizeof(int) * 8 + 1] = "";
     getTwosComplement(firstNumber, firstNumberBinary, sizeof(int) * 8);
-    int secondNumberBinary[sizeof(int) * 8] = { 0 };
+    char secondNumberBinary[sizeof(int) * 8 + 1] = "";
     getTwosComplement(secondNumber, secondNumberBinary, sizeof(int) * 8);
     int thirdNumber = 0;
-    int thirdNumberBinaryToCheck[sizeof(int) * 8] = { 0 };
-    int thirdNumberBinary[sizeof(int) * 8] = { 0 };
+    char thirdNumberBinaryToCheck[sizeof(int) * 8 + 1] = "";
+    char thirdNumberBinary[sizeof(int) * 8 + 1] = "";
+    getTwosComplement(thirdNumber, thirdNumberBinary, sizeof(int) * 8);
+    getTwosComplement(thirdNumber, thirdNumberBinaryToCheck, sizeof(int) * 8);
     getSum(firstNumberBinary, secondNumberBinary, thirdNumberBinary, sizeof(int) * 8);
     if (!isTwoArraysEqual(thirdNumberBinary, thirdNumberBinaryToCheck, sizeof(int) * 8))
     {
@@ -118,27 +125,29 @@ int getSumTest(void)
     getTwosComplement(secondNumber, secondNumberBinary, sizeof(int) * 8);
     thirdNumber = 11;
     getTwosComplement(11, thirdNumberBinaryToCheck, sizeof(int) * 8);
+    getTwosComplement(0, thirdNumberBinary, sizeof(int) * 8);
     getSum(firstNumberBinary, secondNumberBinary, thirdNumberBinary, sizeof(int) * 8);
+    printf("%s, %s\n", thirdNumberBinary, thirdNumberBinaryToCheck);
     if (!isTwoArraysEqual(thirdNumberBinary, thirdNumberBinaryToCheck, sizeof(int) * 8))
     {
         return 2;
     }
-    return 0;
+    return ok;
 }
 
-int toDecimal(int* binary, int size)
+int toDecimal(const char* const binary, const size_t size)
 {
     int number = 0;
     int current = 1;
-    for (int i = size - 1; i > 0; --i)
+    for (size_t i = size - 1; i > 0; --i)
     {
-        if (binary[i] == 1)
+        if (binary[i] == '1')
         {
             number += current;
         }
         current *= 2;
     }
-    if (binary[0] == 1) {
+    if (binary[0] == '1') {
         number *= -1;
     }
     return number;
@@ -146,94 +155,80 @@ int toDecimal(int* binary, int size)
 
 int toDecimalTest(void)
 {
-    int number[11] = {1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1};
+    const char number[11] = "11011101011";
     int decimalNumber = -747;
     if (decimalNumber != toDecimal(number, 11))
     {
         return 1;
     }
-    int secondNumber[11] = { 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1 };
+    const char secondNumber[11] = "01011101011";
     decimalNumber = 747;
     if (decimalNumber != toDecimal(secondNumber, 11))
     {
         return 2;
     }
-    int thirdNumber[16] = { 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1 };
+    const char thirdNumber[16] = "0101110110101011";
     decimalNumber = 23979;
     if (decimalNumber != toDecimal(thirdNumber, 16))
     {
         return 3;
     }
-    return 0;
+    return ok;
 }
 
 int getTwosComplementTest(void)
 {
     int decimalNumber = -1;
-    int twosCompletmentNumber[sizeof(int) * 8] = { 0 };
-    int twosCompletmentNumberForCheck[sizeof(int) * 8] = { 0 };
-    for (int i = 0; i < sizeof(int) * 8; i++)
-    {
-        twosCompletmentNumberForCheck[i] = 1;
-    }
+    char twosCompletmentNumber[sizeof(int) * 8 + 1] = { 0 };
+    const char twosCompletmentNumberForCheck[sizeof(int) * 8 + 1] = "11111111111111111111111111111111";
     getTwosComplement(decimalNumber, twosCompletmentNumber, sizeof(int) * 8);
     if (!isTwoArraysEqual(twosCompletmentNumberForCheck, twosCompletmentNumber, sizeof(int) * 8))
     {
         return 1;
     }
     decimalNumber = -7;
-    int secondTwosCompletmentNumber[sizeof(int) * 8] = { 0 };
-    int secondTwosCompletmentNumberForCheck[sizeof(int) * 8] = { 0 };
-    for (int i = 0; i < sizeof(int) * 8; i++)
-    {
-        secondTwosCompletmentNumberForCheck[i] = 1;
-    }
-    secondTwosCompletmentNumberForCheck[sizeof(int) * 8 - 2] = 0;
-    secondTwosCompletmentNumberForCheck[sizeof(int) * 8 - 3] = 0;
+    char secondTwosCompletmentNumber[sizeof(int) * 8 + 1] = { 0 };
+    const char secondTwosCompletmentNumberForCheck[sizeof(int) * 8 + 1] = "11111111111111111111111111111001";
     getTwosComplement(decimalNumber, secondTwosCompletmentNumber, sizeof(int) * 8);
     if (!isTwoArraysEqual(secondTwosCompletmentNumber, secondTwosCompletmentNumberForCheck, sizeof(int) * 8))
     {
         return 2;
     }
     decimalNumber = 127;
-    int thirdTwosCompletmentNumber[sizeof(int) * 8] = { 0 };
-    int thirdTwosCompletmentNumberForCheck[sizeof(int) * 8] = { 0 };
-    for (int i = 1; i < 8; i++)
-    {
-        thirdTwosCompletmentNumberForCheck[sizeof(int) * 8 - i] = 1;
-    }
+    char thirdTwosCompletmentNumber[sizeof(int) * 8 + 1] = { 0 };
+    const char thirdTwosCompletmentNumberForCheck[sizeof(int) * 8 + 1] = "00000000000000000000000001111111";
     getTwosComplement(decimalNumber, thirdTwosCompletmentNumber, sizeof(int) * 8);
     if (!isTwoArraysEqual(thirdTwosCompletmentNumber, thirdTwosCompletmentNumberForCheck, sizeof(int) * 8))
     {
         return 3;
     }
-    return 0;
+    return ok;
 }
 
 int tests(void)
 {
-    if (getSumTest() != 0)
+    if (getSumTest() != ok)
     {
         return 10 + getSumTest();
     }
-    if (getTwosComplementTest() != 0)
+    if (getTwosComplementTest() != ok)
     {
         return 20 + getTwosComplementTest();
     }
-    if (toDecimalTest() != 0)
+    if (toDecimalTest() != ok)
     {
         return 30 + toDecimalTest();
     }
-    return 0;
+    return ok;
 }
 
-int main()
+int main(void)
 {
     setlocale(LC_ALL, "Russian");
-    int errorCode = tests();
-    if (errorCode != 0)
+    const int errorCode = tests();
+    if (errorCode != ok)
     {
-        printf("ERROR %d\n", errorCode);
+        printf("ERROR IN %s FUNCTION, CASE %d\n", TEST_ERRORS[errorCode / 10], errorCode % 10);
         return errorCode;
     }
     int firstNumber = 0;
@@ -241,55 +236,41 @@ int main()
     if (scanf_s("%d", &firstNumber) != 1)
     {
         printf("ОШИБКА ВВОДА");
-        return 1;
+        return inputError;
     }
     printf("Введите второе число:");
-    int firstBinary[sizeof(int) * 8] = { 0 };
+    char firstBinary[sizeof(int) * 8 + 1] = "";
     getTwosComplement(firstNumber, firstBinary, sizeof(int) * 8);
     int secondNumber = 0;
     if (scanf_s("%d", &secondNumber) != 1)
     {
         printf("ОШИБКА ВВОДА");
-        return 1;
+        return inputError;
     };
-    int secondBinary[sizeof(int) * 8] = { 0 };
+    char secondBinary[sizeof(int) * 8 + 1] = { 0 };
     getTwosComplement(secondNumber, secondBinary, sizeof(int) * 8);
-    printf("Первое число в дополнительном коде:");
-    for (int i = 0; i < sizeof(int) * 8; i++)
-    {
-        printf("%d", firstBinary[i]);
-    }
-    printf("\n");
-    printf("Второе число в дополнительном коде:");
-    for (int i = 0; i < sizeof(int) * 8; ++i)
-    {
-        printf("%d", secondBinary[i]);
-    }
-    int thirdBinary[sizeof(int) * 8] = { 0 };
+    printf("Первое число в дополнительном коде: %s\n", firstBinary);
+    printf("Второе число в дополнительном коде: %s\n", secondBinary);
+    char thirdBinary[sizeof(int) * 8 + 1] = { 0 };
+    getTwosComplement(0, thirdBinary, sizeof(int));
     getSum(firstBinary, secondBinary, thirdBinary, sizeof(int) * 8);
-    printf("\n");
-    printf("Сумма чисел в дополнительном коде:");
-    for (int i = 0; i < sizeof(int) * 8; i++)
-    {
-        printf("%d", thirdBinary[i]);
-    }
-    printf("\n");
-    if (thirdBinary[0] == 1)
+    printf("Сумма чисел в дополнительном коде: %s\n", thirdBinary);
+    if (thirdBinary[0] == '1')
     {
         XOREverythingWithOnes(thirdBinary, sizeof(int) * 8);
-        for (int i = sizeof(int) * 8 - 1; i >= 0; i--)
+        for (int i = sizeof(int) * 8 - 1; i >= 0; --i)
         {
-            if (thirdBinary[i] == 0)
+            if (thirdBinary[i] == '0')
             {
-                thirdBinary[i] = 1;
+                thirdBinary[i] = '1';
                 break;
             }
-            thirdBinary[i] = 0;
+            thirdBinary[i] = '0';
         }
-        thirdBinary[0] = 1;
+        thirdBinary[0] = '1';
     }
     printf("\n");
     printf("Сумма чисел:");
-    printf("%d", toDecimal(thirdBinary, sizeof(int) * 8));
-    return 0;
+    printf("%d\n", toDecimal(thirdBinary, sizeof(int) * 8));
+    return ok;
 }
