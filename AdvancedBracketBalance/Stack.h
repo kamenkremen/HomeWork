2754352abd28661958d6f13da217e4213447c07f
kamenkremen <ivanz0610@gmail.com>
2023-11-25 15:41:28 +0300

    add const to Stack.h

--- AdvancedBracketBalance/Stack.h
+++ AdvancedBracketBalance/Stack.h
@@ -12,12 +12,12 @@ typedef char StackValue;
 //объявление самого стэка
 typedef struct Stack Stack;
 //добавление в стэк элемента
-int push(Stack** stack, const StackValue value);
+int push(Stack** const stack, const StackValue value);
 //удаление верхнего элемента из стэка
-int pop(Stack** stack);
+int pop(Stack** const stack);
 //просмотр верхнего элемента стэка
-StackValue top(Stack* stack, int* errorCode);
+StackValue top(const Stack* const stack, int* const errorCode);
 //освобождение стэка
-void freeStack(Stack** stack);
+void freeStack(Stack** const stack);
 //проверка, пустой ли стэк
-bool isEmpty(Stack* stack);
+bool isEmpty(const Stack* const stack);
