9ab4093df23d69399acdccbfc3c47fe007e419ae
kamenkremen <ivanz0610@gmail.com>
2023-11-29 07:53:27 +0300

    add task AVL Tree

---
+++ AVL/AVL.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <stdbool.h>
+typedef struct Tree Tree;
+bool deleteElement(Tree* const tree, const char* key);
+bool isContains(const Tree* const tree, const char* key);
+int insert(Tree* const tree, const char* value, const char* key);
+char* getValue(const Tree* const tree, const char* key);
+Tree* createTree(void);
+void deleteTree(Tree** const tree);
