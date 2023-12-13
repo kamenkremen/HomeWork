fa551db6f69c3f798d0ff7970f552031cfc52e14
kamenkremen <ivanz0610@gmail.com>
2023-12-13 14:25:10 +0300

    add task 2

---
+++ FinalTestTask2/PriorityQueue.c
@@ -0,0 +1,144 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "PriorityQueue.h"
+#include "ErrorCodes.h"
+
+typedef struct QueueElement
+{
+    QueueValue value;
+    QueuePriority priority;
+} QueueElement;
+
+struct PriorityQueue
+{
+    QueueElement** heap;
+    size_t size;
+    size_t capacity;
+};
+
+static void swap(QueueElement** first, QueueElement** second)
+{
+    QueueElement* third = *first;
+    *first = *second;
+    *second = third;
+}
+
+static QueueErrorCode siftUp(PriorityQueue* queue, size_t index)
+{
+    if (queue == NULL)
+    {
+        return nullPointerError;
+    }
+    for (; index != 0 && queue->heap[index]->priority > queue->heap[(index - 1) / 2]->priority; index = (index - 1) / 2)
+    {
+        swap(&queue->heap[index], &queue->heap[(index - 1) / 2]);
+    }
+    return ok;
+}
+
+static QueueErrorCode siftDown(PriorityQueue* queue, size_t index)
+{
+    if (queue == NULL)
+    {
+        return nullPointerError;
+    }
+    while (2 * index + 1 < queue->size)
+    {
+        size_t leftChild = 2 * index + 1;
+        size_t rightChild = 2 * index + 2;
+        size_t j = leftChild;
+        if (rightChild < queue->size && queue->heap[rightChild]->priority > queue->heap[leftChild]->priority)
+        {
+            j = rightChild;
+        }
+        if (queue->heap[index]->priority >= queue->heap[j]->priority)
+        {
+            break;
+        }
+        swap(&queue->heap[index], &queue->heap[j]);
+        index = j;
+    }
+    return ok;
+}
+
+PriorityQueue* createPriorityQueue(void)
+{
+    PriorityQueue* queue = (PriorityQueue*)calloc(1, sizeof(PriorityQueue));
+    if (queue == NULL)
+    {
+        return NULL;
+    }
+    queue->capacity = 1;
+    queue->heap = (QueueElement**)calloc(1, sizeof(QueueElement));
+    if (queue->heap == NULL)
+    {
+        free(queue);
+        return NULL;
+    }
+    return queue;
+}
+
+QueueErrorCode enqueue(PriorityQueue* const queue, const QueueValue value, const QueuePriority priority)
+{
+    if (queue == NULL)
+    {
+        return nullPointerError;
+    }
+    if (queue->capacity <= queue->size)
+    {
+        queue->capacity *= 2;
+        queue->heap = (QueueElement**)realloc(queue->heap, queue->capacity * sizeof(QueueElement));
+        if (queue->heap == NULL)
+        {
+            return memoryError;
+        }
+    }
+    QueueElement* newElement = (QueueElement*)calloc(1, sizeof(QueueElement));
+    if (newElement == NULL)
+    {
+        return memoryError;
+    }
+    newElement->value = value;
+    newElement->priority = priority;
+    queue->heap[queue->size] = newElement;
+    siftUp(queue, queue->size);
+    ++queue->size;
+    return ok;
+}
+
+QueueValue dequeue(PriorityQueue* const queue, QueueErrorCode* const errorCode)
+{
+    if (queue == NULL)
+    {
+        *errorCode = nullPointerError;
+        return nullPointerError;
+    }
+    if (queue->size == 0)
+    {
+        *errorCode = indexOutOfRangeError;
+        return indexOutOfRangeError;
+    }
+    QueueElement* maximum = queue->heap[0];
+    queue->heap[0] = queue->heap[queue->size - 1];
+    *errorCode = siftDown(queue, 0);
+    --queue->size;
+    QueueValue value = maximum->value;
+    free(maximum);
+    return value;
+}
+
+void deleteQueue(PriorityQueue** const queue)
+{
+    if (queue == NULL)
+    {
+        return;
+    }
+    int errorCode = ok;
+    for (size_t i = 0; i < (*queue)->size; ++i)
+    {
+        dequeue(*queue, &errorCode);
+    }
+    free((*queue)->heap);
+    free(*queue);
+}
---
+++ FinalTestTask2/main.c
@@ -0,0 +1,19 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+#include "PriorityQueue.h"
+#include "ErrorCodes.h"
+
+int main(void)
+{
+    int errorCode = tests();
+    if (errorCode != ok)
+    {
+        return errorCode;
+    }
+    else
+    {
+        printf("All tests has been successfully passed.\n");
+    }
+    return ok;
+}
